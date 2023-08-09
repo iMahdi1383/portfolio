c45c563ccdaa782e08535e4232d53422fdbea3b7
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2023-08-09 13:32:08 +0330

    use Section Wrapper for About Section

--- akolad-portfolio/src/components/About.jsx
+++ akolad-portfolio/src/components/About.jsx
@@ -1,17 +1,17 @@
 import Tilt from 'react-parallax-tilt'
 import {motion} from 'framer-motion'
+import * as PropTypes from "prop-types";
 
 import {styles} from '../styles'
 import {services} from "../constants";
 import {fadeIn, textVariant} from "../utils/motion";
-import * as PropTypes from "prop-types";
-
+import {SectionWrapper} from '../hoc';
 
 const ServicesCard = ({index, title, icon}) => {
   return (
     <motion.div
       variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] rounded-[20px] rounded-tl-[200px] w-full green-pink-gradient p-[1px] shadow-card"
+      className=" w-full xs:w-[250px] rounded-[20px] rounded-tl-[200px] green-pink-gradient p-[1px] shadow-card"
     >
       <Tilt
         glareEnable={true}
@@ -43,33 +43,43 @@ const ServicesCard = ({index, title, icon}) => {
   )
 }
 
+ServicesCard.propTypes = {
+  index: PropTypes.number,
+  title: PropTypes.string,
+  icon: PropTypes.string,
+};
 
 const About = () => {
-  return (<>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>
-        من چه کسی هستم؟
-      </p>
-      <h2 className={styles.sectionHeadText}>
-        درباره من
-      </h2>
-    </motion.div>
-    <motion.p
-      variants={fadeIn("", "", 0.1, 1)}
-      className="mt-4 text-secondary text-[16px] max-w-3xl leading-[30px]"
-    >
-      من مهدی عسکرپور، توسعه دهنده نرم افزار و طراح رابط کاربری، متولد 1383، مجرد و در حال حاضر دانشجوی رشته نرم افزار
-      هستم. از 13 سالگی به برنامه نویسی علاقمند شدم و در حوزه های مختلف توسعه نرم افزار فعالیت هایی داشتم. از زبان های
-      سطح پایین مثل C برای برنامه نویسی میکروکنترولر ها، تا زبان های سطح بالا مثل Python ، PHP و C# و Dart و C++ و
-      JavaScript و ... تجربیاتی دارم. اما پس از یافتن گرایش مورد علاقه خودم، به سمت طراحی و توسعه نرم افزارهای Cross
-      Platform و خصوصا سمت Frontend سوق یافتم.
-    </motion.p>
-    <div className="mt-20 flex flex-wrap gap-10">
-      {services.map((service, index) => (<ServicesCard
-        key={service.title}
-        index={index} {...service} />))}
-    </div>
-  </>)
+  return (
+    <>
+      <motion.div
+        variants={textVariant()}
+      >
+        <p className={styles.sectionSubText}>
+          من چه کسی هستم؟
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          درباره من
+        </h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.5, 2)}
+        className="mt-4 text-secondary text-[16px] max-w-3xl leading-[30px]"
+      >
+        من مهدی عسکرپور، توسعه دهنده نرم افزار و طراح رابط کاربری، متولد 1383، مجرد و در حال حاضر دانشجوی رشته نرم افزار
+        هستم. از 13 سالگی به برنامه نویسی علاقمند شدم و در حوزه های مختلف توسعه نرم افزار فعالیت هایی داشتم. از زبان های
+        سطح پایین مثل C برای برنامه نویسی میکروکنترولر ها، تا زبان های سطح بالا مثل Python ، PHP و C# و Dart و C++ و
+        JavaScript و ... تجربیاتی دارم. اما پس از یافتن گرایش مورد علاقه خودم، به سمت طراحی و توسعه نرم افزارهای Cross
+        Platform و خصوصا سمت Frontend سوق یافتم.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap justify-center content-center gap-10 me-3">
+        {services.map((service, index) => (<ServicesCard
+          key={service.title}
+          index={index} {...service} />))}
+      </div>
+    </>
+  )
 }
 
-export default About
+const sectionWrapper = SectionWrapper(About, "about");
+export default sectionWrapper;
